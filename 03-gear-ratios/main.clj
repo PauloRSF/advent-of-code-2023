3b1bade01fd5a2045bef79bae89b7bffb851ea5d
PauloRSF <paulorsouzaf@gmail.com>
2023-12-25 22:54:10 -0300

    Improve day 3 solution a bit (it's broken now tho)

--- 03-gear-ratios/main.clj
+++ 03-gear-ratios/main.clj
@@ -8,72 +8,84 @@
       nine (int \9)]
   (defn is-digit? [c] (<= zero (int c) nine)))
 
-(def is-right-adjacent (fn [lhs rhs] (= (second lhs) (+ 1 (second rhs)))))
+(def is-right-adjacent (fn [lhs rhs] (= (lhs :x) (+ 1 (rhs :x)))))
 
-(def enumerate-string-chars
-  (fn [str]
-    (def enumerated-row-and-columns (map-indexed
-                                     vector
-                                     (map
-                                      (fn [l] (map-indexed vector (char-array l)))
-                                      (clojure.string/split-lines input))))
+(def get-3x3-neighborhood (fn [x y]
+                            (set
+                             [(seq [(- x 1) (- y 1)])
+                              (seq [x (- y 1)])
+                              (seq [(+ x 1) (- y 1)])
+                              (seq [(- x 1) y])
+                              (seq [x y])
+                              (seq [(+ x 1) y])
+                              (seq [(- x 1) (+ y 1)])
+                              (seq [x (+ y 1)])
+                              (seq [(+ x 1) (+ y 1)])])))
 
-    (apply concat (map
-                   (fn [k]
-                     (map
-                      (fn [x] (seq [(first k) (first x) (second x)]))
-                      (second k)))
-                   enumerated-row-and-columns))))
+(def enumerate-lines-chars (fn [x] (map (fn [z] (map-indexed #({:y %1 :ch %2}) (filter #(not= %1 \.) z))) (seq x))))
 
-(def get-adjacents (fn [x y]
-                     (set
-                      [(seq [(- x 1) (- y 1)])
-                       (seq [x (- y 1)])
-                       (seq [(+ x 1) (- y 1)])
-                       (seq [(- x 1) y])
-                       (seq [x y])
-                       (seq [(+ x 1) y])
-                       (seq [(- x 1) (+ y 1)])
-                       (seq [x (+ y 1)])
-                       (seq [(+ x 1) (+ y 1)])])))
+(def add-line-index-to-elements (fn [x] (map-indexed (fn [i z] (map (fn [m] (assoc m :x i)) z)) (seq x))))
+
+(def flatten-elements (partial apply concat))
+
+(def is-gear-entry #(= (%1 :ch) \*))
+
+(def is-symbol-entry #(and (not= (%1 :ch) \.) (not (is-digit? (%1 :ch)))))
+
+(def is-digit-entry #(is-digit? (%1 :ch)))
+
+(def pick-entry-coords #(select-keys %1 [:x :y]))
+
+(def get-entries-coords (comp
+                         #(set %)
+                         #(map pick-entry-coords %)
+                         #(filter is-gear-entry %)))
+
+(def process-input (comp
+                    flatten-elements ;; ((0 0 A), (0 1 B), (1 0 C), (1 1 D))
+                    add-line-index-to-elements ;; (((0 0 A), (0 1 B)), ((1 0 C), (1 1 D)))
+                    enumerate-lines-chars ;; (((0 A), (1 B)), ((0 C), (1 D)))
+                    clojure.string/split-lines)) ;; [AB CD]
 
 (defn main []
   (def input (slurp "input.txt"))
 
-  (def enumerated-chars (enumerate-string-chars input))
+  (def enumerated-chars (process-input input))
 
-  (def symbols (filter (fn [x] (def k (nth x 2)) (and (not= k \.) (not (is-digit? k)))) enumerated-chars))
+  (def symbol-entries (filter is-symbol-entry enumerated-chars))
 
-  (def digits (filter (fn [x] (def k (nth x 2)) (is-digit? k)) enumerated-chars))
+  (def digit-entries (filter is-digit-entry enumerated-chars))
 
   (def asd (reduce
             (fn [acc k]
               (if (is-right-adjacent k (first (first acc)))
                 (conj (drop 1 acc) (conj (first acc) k))
                 (conj acc (seq [k]))))
-            (seq [(seq [(first digits)])])
-            (drop 1 digits)))
+            (seq [(seq [(first digit-entries)])])
+            (drop 1 digit-entries)))
 
   (def qwe (map (fn [x]
                   (reduce
                    (fn [acc k]
-                     (seq [(+ (first acc) (* (int (pow 10 (first k))) (Character/digit (nth (second k) 2) 10))) (union (get-adjacents (first (second k)) (second (second k))) (second acc))]))
+                     (seq [(+ (first acc) (* (int (pow 10 (first k))) (Character/digit (nth (second k) 2) 10))) (union (get-3x3-neighborhood (first (second k)) (second (second k))) (second acc))]))
                    (seq [0 (set [])])
                    (map-indexed vector x)))
                 asd))
 
-  (def dit (set (map (fn [x] (seq [(first x) (second x)])) symbols)))
+  (def symbol-coords (get-entries-coords symbol-entries))
 
-  (def ant (filter (fn [k] (not (empty? (intersection dit (second k))))) qwe))
-
-  (def nic (set (map (fn [x] (seq [(first x) (second x)])) (filter (fn [k] (= (nth k 2) \*)) symbols))))
-
-  (def abn (filter (fn [m] (= 2 (count m))) (map (fn [k] (filter (fn [x] (contains? (second x) k)) qwe)) nic)))
+  (def ant (filter (fn [k] (not (empty? (intersection symbol-coords (second k))))) qwe))
 
   (def sum-of-part-numbers (reduce (fn [acc k] (+ acc (first k))) 0 ant))
-  (def sum-of-gear-ratios (reduce (fn [acc k] (+ acc (* (first (first k)) (first (second k))))) 0 abn))
 
   (println "Sum of all part numbers:" sum-of-part-numbers)
+
+  (def gear-coords (get-entries-coords (filter is-gear-entry symbol-entries)))
+
+  (def abn (filter (fn [m] (= 2 (count m))) (map (fn [k] (filter (fn [x] (contains? (second x) k)) qwe)) gear-coords)))
+
+  (def sum-of-gear-ratios (reduce (fn [acc k] (+ acc (* (first (first k)) (first (second k))))) 0 abn))
+
   (println "Sum of all gear ratios:" sum-of-gear-ratios))
 
 (main)
