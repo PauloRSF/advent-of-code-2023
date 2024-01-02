ff0971ab9723a6654d804f8a5aa67d57ae48c4df
PauloRSF <paulorsouzaf@gmail.com>
2024-01-02 16:36:14 -0300

    Start using a smarter approach on day 5

--- 05-if-you-give-a-seed-a-fertilizer/main.erl
+++ 05-if-you-give-a-seed-a-fertilizer/main.erl
@@ -40,18 +40,18 @@ parse_input(InputData) ->
 should_map_seed_number(SeedNumber, MapEntry) ->
   (SeedNumber >= MapEntry#mapEntry.origin) and (SeedNumber < MapEntry#mapEntry.origin + MapEntry#mapEntry.size).
 
-map_seed_number(SeedNumber, MapEntry) ->
+map_seed_number_with_entry(SeedNumber, MapEntry) ->
   MapEntry#mapEntry.destination + (SeedNumber - MapEntry#mapEntry.origin).
 
-trm(SeedNumber, MapEntries) ->
-  R = lists:search(fun(MapEntry) -> should_map_seed_number(SeedNumber, MapEntry) end, MapEntries),
-  case R of
-    {value, MapEntry} -> map_seed_number(SeedNumber, MapEntry);
+map_seed_number(SeedNumber, Map) ->
+  SearchResult = lists:search(fun(MapEntry) -> should_map_seed_number(SeedNumber, MapEntry) end, Map),
+  case SearchResult of
+    {value, MapEntry} -> map_seed_number_with_entry(SeedNumber, MapEntry);
     _ -> SeedNumber
   end.
 
 map_seed_to_location(SeedNumber, Maps) -> 
-  lists:foldl(fun(X, Acc) -> trm(Acc, X) end, SeedNumber, Maps).
+  lists:foldl(fun(Map, Acc) -> map_seed_number(Acc, Map) end, SeedNumber, Maps).
 
 map_seed_list_to_locations(SeedNumbers, Maps) ->
   lists:map(fun(X) -> map_seed_to_location(X, Maps) end, SeedNumbers).
@@ -59,14 +59,56 @@ map_seed_list_to_locations(SeedNumbers, Maps) ->
 map_seed_numbers_to_ranges([Start, Length | SeedNumbers]) -> [{Start, Length} | map_seed_numbers_to_ranges(SeedNumbers)];
 map_seed_numbers_to_ranges([]) -> [].
 
-kvk({Start, Length}, Maps) ->
-  SeedNumbers = lists:seq(Start, Start + Length - 1),
-  SeedLocations = map_seed_list_to_locations(SeedNumbers, Maps),
-  lists:min(SeedLocations).
+mz(SeedNumbers, Maps) ->
+  lists:foldl(fun(X, Acc) -> lists:min([map_seed_to_location(X, Maps), Acc]) end, 999999999999999999, SeedNumbers).
+
+ptn({Start, Length}, MapEntry) ->
+  End = Start + Length,
+  EntryEnd = MapEntry#mapEntry.origin + MapEntry#mapEntry.size,
+
+  StartsAfter = Start > MapEntry#mapEntry.origin,
+  StartsBefore = Start < MapEntry#mapEntry.origin,
+  StartsBetween = (Start > MapEntry#mapEntry.origin) and (Start < EntryEnd),
+
+  EndsAfter = End > EntryEnd,
+  EndsBefore = End < EntryEnd,
+  EndsBetween = (End > MapEntry#mapEntry.origin) and (End < EntryEnd),
+
+  DoesNotIntersect = (End < MapEntry#mapEntry.origin) or (Start > (MapEntry#mapEntry.origin + MapEntry#mapEntry.size)),
+  IsStartIntersecting = StartsBefore and EndsBetween,
+  IsEndIntersecting = StartsBetween and EndsAfter,
+  DoesContain = StartsBefore and EndsAfter,
+  IsContained = StartsAfter and EndsBefore,
+
+  if
+    DoesNotIntersect -> [{Start, Length}];
+    IsStartIntersecting ->
+      [{Start, (MapEntry#mapEntry.origin - Start)}, {MapEntry#mapEntry.destination, Length - (MapEntry#mapEntry.origin - Start)}];
+    IsEndIntersecting ->
+      [{}];
+    DoesContain ->
+      [{}];
+    IsContained ->
+      [{}];
+  end.
+
+qze([SeedRange | SeedRanges], [MapEntry, MapEntries]) -> [ptn(SeedRange, MapEntry) | ]
+
+abd(SeedRanges, [Map | Maps]) -> 
+  M = qze(SeedRanges, Map),
+  abd(M, Maps).
+abd(SeedRange, []) -> 
+
+kvk({_, 0}, _, Min) -> Min;
+kvk({Start, Length}, Maps, Min) ->
+  T = Start + Length - 1,
+  K = map_seed_to_location(T, Maps),
+  X = lists:min([Min, K]),
+  Y = Length - 1,
+  kvk({Start, Y}, Maps, X).
 
 mkd(SeedRanges, Maps) -> 
-  X = lists:map(fun(X) -> kvk(X, Maps) end, SeedRanges),
-  lists:min(X).
+  lists:foldl(fun(X, Acc) -> lists:min([kvk(X, Maps, 999999999999999999), Acc]) end, 999999999999999999, SeedRanges).
 
 main() ->
   {ok, InputData} = file:read_file("input.txt"),
@@ -76,3 +118,7 @@ main() ->
   L = map_seed_numbers_to_ranges(SeedNumbers),
   X = mkd(L, Maps),
   io:fwrite("Smallest location for seed ranges: ~w~n", [X]).
+
+%SeedNumbers = lists:seq(Start, Start + Length - 1),
+  %lists:foldl(fun(X, Acc) -> lists:min([map_seed_to_location(X, Maps), Acc]) end, 999999999999999999, lists:seq(Start, Start + Length - 1)).
+%mz(SeedNumbers, Maps);
