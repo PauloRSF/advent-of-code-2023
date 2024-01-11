6fb0079028aa7f624644a196742ba2ef707c11e8
PauloRSF <paulorsouzaf@gmail.com>
2024-01-10 23:28:45 -0300

    Add input parsing to day 7 solution

--- 07-camel-cards/main.go
+++ 07-camel-cards/main.go
@@ -1,7 +1,96 @@
 package main
 
+import "os"
 import "fmt"
+import "bufio"
+import "strings"
+import "strconv"
+
+type HandType = uint8
+type CardStrength = uint8
+
+const (
+	HighCard HandType = iota
+	OnePair
+	TwoPair
+	ThreeOfAKind
+	FullHouse
+	FourOfAKind
+	FiveOfAKind
+)
+
+type Hand struct {
+	cardStrengths []CardStrength
+	typ HandType
+	bid uint
+}
+
+func mapCardToStrength(card rune) CardStrength {
+	if (card >= '2' && card <= '9') {
+		return uint8(card) - '2'
+	}
+
+	if (card == 'T') {
+		return 10
+	}
+	
+	if (card == 'J') {
+		return 11
+	}
+	
+	if (card == 'Q') {
+		return 12
+	}
+	
+	if (card == 'K') {
+		return 13
+	}
+	
+	if (card == 'A') {
+		return 14
+	}
+
+	return 0
+}
+
+func getHandType(cards []CardStrength) HandType {
+	return FiveOfAKind
+}
+
+func parseHand(handString string) Hand {
+	splitHandString := strings.Split(handString, " ")
+
+	var cardStrengths []CardStrength
+
+	for _, card := range splitHandString[0] {
+		cardStrengths = append(cardStrengths, mapCardToStrength(card))
+	}
+
+	bid, _ := strconv.Atoi(splitHandString[1])
+
+	handType := getHandType(cardStrengths)
+
+	return Hand{cardStrengths, handType, uint(bid)}
+}
+
+func readInput(filePath string) []Hand {
+	file, _ := os.Open(filePath)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var hands []Hand
+
+	for scanner.Scan() {
+		parsedHand := parseHand(scanner.Text())
+		hands = append(hands, parsedHand)
+	}
+
+	return hands
+}
 
 func main() {
-	fmt.Println("Hello")
+	hands := readInput("input.txt");
+
+	fmt.Println(hands)
 }
